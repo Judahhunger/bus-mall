9d78c80dc959c97545c7a8ee14ce846756f41201
Judah Hunger <judah.hunger@gmail.com>
2018-02-19 14:19:30 -0800

    added constructor and 3 instances

--- js/app.js
+++ js/app.js
@@ -1,2 +1,29 @@
 'use strict';
 
+var totalClicks = 0;
+busProduct.allProducts = [];
+
+function busProduct(name, filepath){
+  this.name = name;
+  this.filepath = filepath;
+  this.clicks = 0;
+  this.shown = 0;
+  busProduct.allProducts.push(this);
+}
+
+
+new busProduct('bag', '../img/bag.jpg');
+new busProduct('banana', '../img/banana.jpg');
+new busProduct('bathroom', '../img/bathroom.jpg');
+
+var busImage1 = document.getElementById('product-1');
+var busImage2 = document.getElementById('product-2');
+var busImage3 = document.getElementById('product-3');
+
+function randomProduct(){
+  var makeProduct = Math.floor(Math.random() * busProduct.allProducts.length);
+
+  busImage1.scr = busProduct.allProducts[makeProduct].filepath;
+
+}
+randomProduct();
